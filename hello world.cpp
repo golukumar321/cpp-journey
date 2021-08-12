aa808335cf33e0ef17162d31db7e85a7ab782cd2
golukumar321 <88841583+golukumar321@users.noreply.github.com>
2021-08-12 07:32:06 -0700

    Add files via upload

---
+++ hello world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!"<<endl;
+  cout<<"learn in c++";
+  int a = 69;
+  int b = 69;
+  int c = a + b;
+  cout<<"The value of a + b  is :"<<c<<endl;
+  return (0);
+}
