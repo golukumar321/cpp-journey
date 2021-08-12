73fb711f054ca0e065ad76d5b6f9f35aaacfb727
golukumar321 <88841583+golukumar321@users.noreply.github.com>
2021-08-12 07:55:31 -0700

    Add files via upload

---
+++ DATATYPESTRING.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;  /*ABCDEFGHIJKL */
+
+int main() {
+  int a = 66;/*declares integer variable*/
+  float b= 77.6; /*declares float variable*/
+  char c = 'A' ; /*declares a character variable*/ 
+  char name[11] = "Golu Kumar" ; /*Declares a string*/
+  cout<<"the value in a : "<<a<<endl<<"The value in b :"<<b<<endl<<"The value in c: "<<c<<endl; /*Prints all the variables*/
+  cout<<"My name is : "<< name<<endl;
+  return (0);
+}
