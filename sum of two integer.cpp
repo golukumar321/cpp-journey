12b5626536b14f9baa3cd473a0de1dbb39f19b19
golukumar321 <88841583+golukumar321@users.noreply.github.com>
2021-08-12 17:52:35 -0700

    Add files via upload

---
+++ sum of two integer.cpp
@@ -0,0 +1,11 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b,c;/*declares a variable*/
+	cout <<"enter a two number:";
+	cin>>a>>b;
+	c= a+b;
+	cout <<"sum of two number is :"<<c;
+	return (0);
+}
