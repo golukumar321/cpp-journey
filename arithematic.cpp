78e8ae1925343b16cb14d6cb4b17ef3663d20af4
golukumar321 <88841583+golukumar321@users.noreply.github.com>
2021-08-25 06:34:06 -0700

    Add files via upload

---
+++ arithematic.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int e= 99;
+	int f= 55;
+	int addition =e+f;
+	int substraction= e-f;
+	int multipliction = e*f;
+	int  divided = e/f;
+	int  modulus= e%f;
+	printf ("the addition is %d\n,the substraction is %d\n,the multipliction is%d\n,the divided is%d\n,the modulus is%d\n",addition, substraction, multipliction, divided, modulus); //Arithmetic Operators)
+	return 0;
+	
+}
